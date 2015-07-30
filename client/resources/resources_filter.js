2835cec7cb76b093d30f7bf3317f4224c80ae804
eric glassman <eglassman@scitent.com>
2015-07-30 15:56:11 -0400

    filters

--- client/resources/resources_filter.js
+++ client/resources/resources_filter.js
@@ -1,4 +1,4 @@
-Template.measures_filter.rendered = function(){
+Template.resources_filter.rendered = function(){
 
   var tags = Boks.find({ $or: [{ _id: BOK.current()._id }, { $and: [{ancestors: BOK.current()._id}, {public: true}] }] }).fetch();
   var formattedTags = _.map(tags, function(tag){
@@ -18,11 +18,11 @@ Template.measures_filter.rendered = function(){
     return tag;
   });
 
-  if(Session.get('current_measure_filter') === undefined){
-    Session.set('current_measure_filter', BOK.current()._id);
+  if(Session.get('current_resource_filter') === undefined){
+    Session.set('current_resource_filter', BOK.current()._id);
   }
 
-  $("#measures-filter").selectize({
+  $("#resources-filter").selectize({
     plugins: ['remove_button'],
     placeholder: "Filter by tag...",
     create: false,
@@ -39,15 +39,15 @@ Template.measures_filter.rendered = function(){
       }
     },
     options: formattedTags,
-    items: [ Session.get('current_measure_filter') ]
+    items: [ Session.get('current_resource_filter') ]
   });
 
-  var filterSelectizeAPI = $('#measures-filter')[0].selectize;
+  var filterSelectizeAPI = $('#resources-filter')[0].selectize;
 
   filterSelectizeAPI.on("item_add", function(value, $item){
     var _id = value;
 
-    Session.set("current_measure_filter", _id);
+    Session.set("current_resource_filter", _id);
 
   });
 
