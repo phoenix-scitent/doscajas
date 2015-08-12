3673635381a8046ad87a4ace39cdda28345febf6
Timothy Johnson <jhnsntmthy@gmail.com>
2015-08-12 19:55:19 -0400

    refactored the embeded resources to use embedly oembed via jquery

--- client/attempt/embedded_resource.js
+++ client/attempt/embedded_resource.js
@@ -10,4 +10,5 @@ Template.embedded_resource.rendered = function(){
   script.setAttribute('src', '//cdn.embedly.com/widgets/platform.js');
   script.setAttribute('charset', 'UTF-8');
   document.getElementsByTagName('body')[0].appendChild(script);
+  $('a.embedly-oembed').embedly({query: {maxwidth:600,luxe:1} });
 };
