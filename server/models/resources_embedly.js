f4af455c79aef1d4eca3cdfa08cbd0fa4f6f585a
Timothy Johnson <jhnsntmthy@gmail.com>
2015-08-12 22:33:32 -0400

    added frame_embeddable to the resources, so that we can tell if the x-frame-options is set on the resource, then we wont try to embed that resource in an iframe

--- server/models/resources_embedly.js
+++ server/models/resources_embedly.js
@@ -3,26 +3,34 @@ Meteor.methods({
     var extractBase = 'http://api.embed.ly/1/extract';
     var embedlyKey = '9e68b536c1e4495ba32ea615a83fa676';
     console.log("EMBEDLY for: "+url);
-    var result = Meteor.http.get(extractBase, {
-      params: {
-        key: embedlyKey,
-        url: url
-      }});
-    if(result.statusCode==200) {
-      var respJson = JSON.parse(result.content);
-      return Meteor.call("embedlyIntoResource", respJson, resource_id);
+    var geturl = Meteor.http.get(url, {});
+    if (geturl.statusCode==200) {
+      console.log(geturl.headers['x-frame-options']);
+      var embeddable = geturl.headers['x-frame-options'] === undefined;
+      var result = Meteor.http.get(extractBase, {
+        params: {
+          key: embedlyKey,
+          url: url
+        }});
+      if(result.statusCode==200) {
+        var respJson = JSON.parse(result.content);
+        return Meteor.call("embedlyIntoResource", respJson, resource_id, embeddable);
+      } else {
+        console.log("Embedly issue: ", result.statusCode);
+        var errorJson = JSON.parse(result.content);
+        throw new Meteor.Error(result.statusCode, errorJson.error);
+      }
     } else {
-      console.log("Embedly issue: ", result.statusCode);
-      var errorJson = JSON.parse(result.content);
-      throw new Meteor.Error(result.statusCode, errorJson.error);
+      Resources.update(resource_id, {$set: {not_a_url: true}});
+      throw new Meteor.Error(geturl.statusCode, "Is not a valid URL");
     }
   },
-  embedlyIntoResource: function(embed, resource_id) {
+  embedlyIntoResource: function(embed, resource_id, embeddable) {
     var resource = Resources.findOne(resource_id);
     var title = resource.title || embed.title;
     var image = resource.image_url || embed.images[0].url;
     var desc = resource.description || embed.description;
-    return Resources.update(resource_id, {$set: {embedly: embed, title: title, description: desc, image_url: image}});
+    return Resources.update(resource_id, {$set: {embedly: embed, title: title, description: desc, image_url: image, frame_embeddable: embeddable}});
   },
   refreshEmbedly: function() {
     _.each(Resources.find().fetch(), function(re) {
