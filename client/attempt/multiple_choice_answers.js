2392bbafb78f5b39317968fc0133a05eb8f25b8e
Timothy Johnson <jhnsntmthy@gmail.com>
2015-08-14 00:29:29 -0400

    updated templates

--- client/attempt/multiple_choice_answers.js
+++ client/attempt/multiple_choice_answers.js
@@ -8,7 +8,7 @@ Template.multiple_choice_answers.helpers({
 });
 
 Template.multiple_choice_answers.events({
-  'click .answer-option': function(event){
+  'click .mc_answer': function(event){
     var $el = $(event.target);
         measure_id = $el.attr('name');
         answer_id = $el.val() - 1;
@@ -54,28 +54,3 @@ Template.multiple_choice_answers.events({
   }
 });
 
-Template.multiple_choice_answers_review.helpers({
-  isCorrect: function(helperParams){
-    var params = helperParams.hash;
-
-    var measure = Measures.findOne({ _id: params.measureId });
-
-    var answer = _.find(measure.answers, function(answer){ return answer.id === params.answerChoiceId });
-
-    return answer.correct;
-  },
-  isYourAnswer: function(helperParams){
-    var params = helperParams.hash;
-
-    var currentAttempt = Sequences.findOne({ _id: Session.get('currentAttemptId') });
-
-    var yourItem = _.find(currentAttempt.attempt.items, function(item){ return item.data._id === params.measureId });
-
-    return (params.answerChoiceId === (yourItem.answer_id + 1));
-  },
-  displayResource: function(helperParams){
-    var params = helperParams.hash;
-
-    return Resources.findOne({ _id: params.resourceId }).title;
-  }
-});
